029d5ca8ecc3be30fc74c1b72536081b350b309e
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-19 22:49:18 +0530

    Update duplicationElement.cpp

--- Array/duplicationElement.cpp
+++ Array/duplicationElement.cpp
@@ -13,21 +13,23 @@ OUTPUT
 using namespace std;
 int main()
 {
- int num,i;
- cin >> num;
- int arr[num];
- 
- for( i=0;i<num;i++)
- {
-     cin >> arr[i];
- }
-  if(arr[i] == i)
-     {
-         cout << i << " " ;
-     }
-    else
+  int num,i;
+  cin >> num;
+  int arr[num];
+
+  for( i=0;i<num;i++)
+  {
+      cin >> arr[i];
+  }
+  for(i=0;i<num;i++)
+  {
+   for(j=i+1;j<num;j++)
     {
-        cout << -1;
+     if(arr[i] == arr[j]
+        {
+         cout << arr[i];
+        }
     }
+  }
     return 0;
 }
