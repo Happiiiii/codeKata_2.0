a160bac8306b7c734babce10a461ed268a81a905
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-21 07:59:04 +0530

    Update Factorial.cpp

--- AlphaTKG/Factorial.cpp
+++ AlphaTKG/Factorial.cpp
@@ -1,5 +1,7 @@
 /*
 Factorial number
+5
+120
 */
 
 #include<iostream>
@@ -14,4 +16,35 @@ cin >> num;
      fact = fact * i;
     }
  cout << fact;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+/*
+factorial using recrusion
+5 
+120
+*/
+
+#include <iostream>
+using namespace std;
+
+//Factorial function
+int f(int n)
+{
+   if (n <= 1)
+   {
+        return 1;
+   }
+   else 
+   {
+       return n*f(n-1);
+   }
+}
+
+int main()
+{
+   int n;
+   cin>>n;
+   cout<<f(n);
+   return 0;
 }
