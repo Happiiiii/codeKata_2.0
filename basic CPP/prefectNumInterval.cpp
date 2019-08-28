6239d37057892da0fdf7beba55fba3b1e1f76409
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-08-28 10:00:18 +0530

    Create prefectNumInterval.cpp

---
+++ basic CPP/prefectNumInterval.cpp
@@ -0,0 +1,35 @@
+/*
+Write a program to print all Perfect numbers between 1 to n.
+1 1000                                                                                                                         
+6                                                                                                                              
+28                                                                                                                             
+496 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j, a, b, sum;
+    cin>>a>>b;
+
+    for(i=a; i<=b; i++)
+    {
+        sum = 0;
+        for(j=1; j<i; j++)
+        {
+            if(i % j == 0)
+            {
+                sum = sum + j;
+            }
+        }
+ 
+        if(sum == i)
+        {
+           cout<<i<<endl;
+        }
+    }
+
+    return 0;
+}
