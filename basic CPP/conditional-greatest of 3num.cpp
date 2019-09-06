5e9eaa0bec39ea01a6b867a44f06aff0c6b1eb6f
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-09-06 16:04:22 +0530

    Update conditional-greatest of 3num.cpp

--- basic CPP/conditional-greatest of 3num.cpp
+++ basic CPP/conditional-greatest of 3num.cpp
@@ -7,14 +7,10 @@ using namespace std;
 
 int main()
 {
-    int num1, num2,num3,largest;
-
-    cout<<"Enter three numbers: ";
-    cin >> num1 >> num2>>num3;
-
-    largest = (num1 > num2 && num1>num3 )? num1:(num2>num3) ? num2 : num3; 
-    
-    cout << largest << " is the largest number.";
+    int a,b,c,max;
+    cin >>a>>b>>c;
+    max = ( a>b && a>c ) ? a : ( b>c ) ? b : c; 
+    cout << max;
 
     return 0;
 }
