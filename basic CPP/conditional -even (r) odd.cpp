0c3c46f6b25493f18a64449397e3c511d1df47cb
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-09-06 16:37:21 +0530

    Update conditional -even (r) odd.cpp

--- basic CPP/conditional -even (r) odd.cpp
+++ basic CPP/conditional -even (r) odd.cpp
@@ -1,15 +1,1 @@
-/*
-4 
-even
-*/
-#include <iostream>
-using namespace std;
 
-int main()
-{
-  int num;
-  cin>> num;
-  /*---------if-------------|---else------*/
-  (num%2==0) ? cout<<"even" : cout<<"odd"; 
-  return 0;
-}
