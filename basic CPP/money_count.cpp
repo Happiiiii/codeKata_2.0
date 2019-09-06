a5e58472cc489ac5843da91c1964424cefca56a4
surya_j <52240799+Happiiiii@users.noreply.github.com>
2019-09-06 11:08:22 +0530

    Update money_count.cpp

--- basic CPP/money_count.cpp
+++ basic CPP/money_count.cpp
@@ -2,21 +2,32 @@
 4000
 8
 */
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-  int amt;
-  cin>>amt;
-  int a500,a100,a50,a10;
-  a500=a100=a50=a10=0;
-  if(amt>500)
-  {
-      a500=amt/500;
-      amt = amt - (a500*500);
-  }
-    cout<<a500;
-    return 0;
+int amt;
+cin>>amt;
+int a500,a100,a50;
+a500=a100=a50=0;
+if(amt>=500)
+{
+a500=amt/500;
+amt=amt%500;
+}
+if(amt>=100)
+{
+    a100=amt/100;
+    amt=amt%100;
+}
+if(amt>=50)
+{
+ a50=amt/50;
+ amt=amt%50;
+}
+
+cout<<a500<<" "<<a100<<" "<<a50;
 }
